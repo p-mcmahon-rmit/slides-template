8653bd1d1e29b746c03830f8d4b76a51a94c4239
hase <patrick.mcmahon@rmit.edu.au>
2025-12-24 16:07:16 +1100

    messing with nav display

--- slideNavigation.js
+++ slideNavigation.js
@@ -1,10 +1,11 @@
 /* ------------------------------------------------------------------------------------------------------------------ */
 // Variables
 /* ------------------------------------------------------------------------------------------------------------------ */
-const nextSlideButton = document.getElementById("nextSlideButton");
-const prevSlideButton = document.getElementById("prevSlideButton");
-const slideLinkWrapper = document.querySelector(".slideLinks");
-const currentSlideDisplay = document.getElementById("currentSlideDisplay");
+const nextSlideButton = document.getElementById("next-slide-button");
+const prevSlideButton = document.getElementById("prev-slide-button");
+const slideLinksContainer = document.querySelector(".slideLinks");
+const slideTitleDisplay = document.getElementById("slide-title-display");
+const currentSlideDisplay = document.getElementById("current-slide-display");
 
 const intersectionObserver = new IntersectionObserver(
     intersectionCallback,
@@ -18,18 +19,32 @@ const slidesInfoArray = slideElementsArray.map((slide, slideNum) => {
     // set slide id
     slide.id = `slide-${slideNum}`;
     slide.dataset.index = slideNum;
+    // find slide title
+    const slideTitle = slide.querySelector("h2").textContent;
     // create nav link
     const slideLink = document.createElement("a");
     slideLink.href = `#slide-${slideNum}`;
     slideLink.textContent = slideNum;
-    slideLinkWrapper.appendChild(slideLink);
+    // when hover display title
+    slideLink.addEventListener("mouseenter", () => {
+        slideTitleDisplay.textContent = `${slideNum}: ${slideTitle}`;
+    });
+    slideLink.addEventListener("mouseleave", () => {
+        slideTitleDisplay.innerHTML = "&nbsp;";
+    })
+    slideLinksContainer.appendChild(slideLink);
+    // observe when new slide more than 50% on screen
     intersectionObserver.observe(slide);
     return {
         index: slideNum,
         navLink: slideLink,
         slide: slide,
+        slideTitle: slideTitle,
     }
 });
+
+// we need to set explicit width to the nav bar for overflow
+
 
 console.log(slidesInfoArray);
 
@@ -42,6 +57,8 @@ let currentSlide = 0;
 /* ------------------------------------------------------------------------------------------------------------------ */
 // Functions
 /* ------------------------------------------------------------------------------------------------------------------ */
+//!!! going from end to start via link
+
 function updateCurrentSlide(newSlideIndex) {
     // make sure within range
     if (newSlideIndex >= 0 && newSlideIndex < slidesInfoArray.length) {
